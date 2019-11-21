f98317944a5b5c130879faefae92f4ae0e4d48ce
marrocamp <marro.comunidades.net@gmail.com>
2019-11-21 15:52:18 -0300

    Add files via upload

---
+++ osrframework/wrappers/instagram.py
@@ -0,0 +1,152 @@
+# !/usr/bin/python
+# -*- coding: cp1252 -*-
+#
+##################################################################################
+#
+#    Copyright 2016 Félix Brezo and Yaiza Rubio (i3visio, contacto@i3visio.com)
+#
+#    This program is part of OSRFramework. You can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##################################################################################
+
+import argparse
+import json
+import re
+import sys
+import urllib2
+
+import osrframework.utils.general as general
+from osrframework.utils.platforms import Platform
+
+class Instagram(Platform):
+    """
+        A <Platform> object for Instagram.
+    """
+    def __init__(self):
+        """
+            Constructor...
+        """
+        self.platformName = "Instagram"
+        self.tags = ["imagery"]
+
+        ########################
+        # Defining valid modes #
+        ########################
+        self.isValidMode = {}
+        self.isValidMode["mailfy"] = True
+        self.isValidMode["phonefy"] = False
+        self.isValidMode["usufy"] = True
+        self.isValidMode["searchfy"] = True
+
+        ######################################
+        # Search URL for the different modes #
+        ######################################
+        # Strings with the URL for each and every mode
+        self.url = {}
+        self.url["usufy"] = "http://www.instagram.com/" + "<usufy>"
+        #self.url["phonefy"] = "http://anyurl.com//phone/" + "<phonefy>"
+        self.url["searchfy"] = "http://picbear.online/search/" + "<searchfy>"
+
+        ######################################
+        # Whether the user needs credentials #
+        ######################################
+        self.needsCredentials = {}
+        #self.needsCredentials["phonefy"] = False
+        self.needsCredentials["mailfy"] = False
+        self.needsCredentials["usufy"] = False
+        self.needsCredentials["searchfy"] = False
+
+        #################
+        # Valid queries #
+        #################
+        # Strings that will imply that the query number is not appearing
+        self.validQuery = {}
+        # The regular expression '.+' will match any query.
+        self.validQuery["mailfy"] = ".+"
+        #self.validQuery["phonefy"] = ".*"
+        self.validQuery["searchfy"] = ".*"
+        self.validQuery["usufy"] = ".+"
+
+        ###################
+        # Not_found clues #
+        ###################
+        # Strings that will imply that the query number is not appearing
+        self.notFoundText = {}
+        #self.notFoundText["phonefy"] = []
+        self.notFoundText["usufy"] = ["Página no encontrada"]
+        self.notFoundText["searchfy"] = []
+
+        #########################
+        # Fields to be searched #
+        #########################
+        self.fieldsRegExp = {}
+
+        # Definition of regular expressions to be searched in phonefy mode
+        #self.fieldsRegExp["phonefy"] = {}
+        # Example of fields:
+        #self.fieldsRegExp["phonefy"]["i3visio.location"] = ""
+
+        # Definition of regular expressions to be searched in usufy mode
+        self.fieldsRegExp["usufy"] = {}
+        # Example of fields:
+        #self.fieldsRegExp["usufy"]["i3visio.location"] = ""
+        # Definition of regular expressions to be searched in searchfy mode
+        self.fieldsRegExp["searchfy"] = {}
+        #"<img alt=\"@([^\"]+)\""
+        self.searchfyAliasRegexp = "\" alt=\"@([^\"]+)\"> <\/div> <div class="
+        # Example of fields:"<img alt=\"@([^\"]+)\""
+
+        ################
+        # Fields found #
+        ################
+        # This attribute will be feeded when running the program.
+        self.foundFields = {}
+
+
+    def check_mailfy(self, query, kwargs={}):
+        """
+        Verifying a mailfy query in this platform.
+
+        This might be redefined in any class inheriting from Platform. The only
+        condition is that any of this should return a dictionary as defined.
+
+        Args:
+        -----
+            query: The element to be searched.
+            kwargs: Dictionary with extra parameters. Just in case.
+
+        Return:
+        -------
+            Returns the collected data if exists or None if not.
+        """
+        import re
+        import requests
+
+        s = requests.Session()
+
+        # Getting the first response to grab the csrf_token
+        r1 = s.get("https://www.instagram.com")
+        csrf_token = re.findall("csrf_token", r1.text)[0]
+
+        # Launching the query to Instagram
+        r2 = s.post(
+            'https://www.instagram.com/accounts/web_create_ajax/attempt/',
+            data={"email": query},
+            headers={"X-CSRFToken": csrf_token}
+        )
+
+        if '{"email": [{"message": "Another account is using' in r2.text:
+            return r2.text
+        else:
+            return None
